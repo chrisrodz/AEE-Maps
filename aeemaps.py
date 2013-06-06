eba8931b77e1a438a2afb6b18e41acdbf9ec40a2
Froilan Irizarry <irizarry.froilan@gmail.com>
2013-06-06 12:44:12 -0400

    Created getAllData and getData(municipio) methods.

--- aeemaps.py
+++ aeemaps.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask import request
+import prepa
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/getdata/municipios', methods=['Get'])
+def getAllData():
+    json_response = prepa.getAll()
+    return json_response
+
+@app.route('/getdata/municipios/<municipio>', methods=['Get'])
+def getData(municipio):
+    if municipio is None:
+        return "error: municipio can't be empty"
+    else:
+        json_response = prepa.getByCity(municipio)
+        return json_response
 
 if __name__ == "__main__":
+    app.debug=True
     app.run()
