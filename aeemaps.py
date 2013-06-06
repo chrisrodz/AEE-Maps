a9dc7a1cbfe9a899826e4faa705c4872f381562b
chrisrodz <christian.etpr10@gmail.com>
2013-06-06 13:49:20 -0400

    Added db models

--- aeemaps.py
+++ aeemaps.py
@@ -1,8 +1,29 @@
 from flask import Flask
 from flask import request
+from flask.ext.sqlalchemy import SQLAlchemy
+
 import prepa
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:////tmp/test.db')
+db = SQLAlchemy(app)
+
+class Area(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pueblo = db.Column(db.String(80), unique=True)
+    name = db.Column(db.String(80), unique=True)
+    def __repr__(self):
+        return "%s: %s" % (self.pueblo,self.name)
+
+class Incidents(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    area_id = db.Column(db.Integer)
+    status = db.Column(db.String(140))
+    last_update = db.Column(db.DateTime)
+    parent_id = db.Column(db.Integer, nullable=True)
+    def __repr__(self):
+        return "%s: %s" % (self.pueblo,self.name)
 
 @app.route('/getdata/municipios', methods=['Get'])
 def getAllData():
